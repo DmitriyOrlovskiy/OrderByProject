bab7ca935a4d206e743966d4a1e51738c387c12a
Dmitriy <dima1999orlovskiy@gmail.com>
2021-11-02 21:15:55 +0200

    init

---
+++ src/OrderByProject/OrderByProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace OrderByProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "Summer Tree      List   Auto Candy   Drug";
+            Console.WriteLine(line);
+            string[] Word = line.Split();
+            //var orderbywords = from word in Word 
+            //orderby word
+            //select word;
+            var orderbywords = Word.OrderBy(c => c).Where(c=>c.Length<6).Select(c => c.Trim());
+                foreach (string word in orderbywords)
+                Console.WriteLine(word);
+        }
+    }
+}
