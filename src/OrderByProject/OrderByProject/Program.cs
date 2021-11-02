cfd8b23fbf920297c84c161bc203db99c42acb59
DmitriyOrlovskiy <93477081+DmitriyOrlovskiy@users.noreply.github.com>
2021-11-02 22:29:57 +0200

    Update Program.cs

--- src/OrderByProject/OrderByProject/Program.cs
+++ src/OrderByProject/OrderByProject/Program.cs
@@ -9,9 +9,9 @@ namespace OrderByProject
         {
             Console.BackgroundColor = ConsoleColor.Gray;
             Console.ForegroundColor = ConsoleColor.Black;
-                string line = "Summer Tree      List   Auto Candy   Drug";
-                    Console.WriteLine(line);
-                string[] Word = line.Split();
+          string line = "Summer Tree      List   Auto Candy   Drug";
+            Console.WriteLine(line);
+          string[] Word = line.Split();
             var orderbywords = Word.OrderBy(c => c).Where(c=>c.Length<6).Select(c => c.Trim());
                 foreach (string word in orderbywords)
                     Console.WriteLine(word);
