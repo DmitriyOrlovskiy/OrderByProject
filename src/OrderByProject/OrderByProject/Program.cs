11252e57bc7351a49d9bce55095b0a92e6669b9e
DmitriyOrlovskiy <93477081+DmitriyOrlovskiy@users.noreply.github.com>
2021-11-09 23:18:27 +0200

    Update Program.cs

--- src/OrderByProject/OrderByProject/Program.cs
+++ src/OrderByProject/OrderByProject/Program.cs
@@ -10,9 +10,11 @@ namespace OrderByProject
          string line = "Summer Tree      List   Auto Candy   Drug";
             Console.WriteLine(line);
          string[] Word = line.Split();
-         var orderbywords = Word.OrderBy(c => c).Where(c=>c.Length<6).Select(c => c.Trim());
+         var orderbywords = Word.OrderBy(c => c).Select(c => c.Trim());
+            {
             foreach (string word in orderbywords)
                 Console.WriteLine(word);
+            }
         }
     }
 }
